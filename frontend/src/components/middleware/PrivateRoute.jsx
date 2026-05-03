a6aa2743140d89f62e387f4bd018b4a24a54eeac
unknown <salmanafzal8282@gmail.com>
2026-05-03 18:01:51 +0500

    Initial commit - Laravel + React Image Marketplace by Muhammad Suleman

--- frontend/src/components/middleware/PrivateRoute.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
-import { Navigate } from 'react-router'
-
-export default function PrivateRoute({children}) {
-    const { isLoggedIn } = useSelector(state => state.user)
-
-    return isLoggedIn ? children : <Navigate to="/login" replace /> 
-}
